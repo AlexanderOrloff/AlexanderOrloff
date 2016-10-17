7efb6e8a6ceb62f1abf6336e22f18168edcad520
AlexanderOrloff <crazymusik98@gmail.com>
2016-10-17 20:13:19 +0300

    Add files via upload

---
+++ hwPython1/python1.py
@@ -0,0 +1,11 @@
+a = int(input('введи а'))
+b = int(input('введи b'))
+c = int(input('введи с'))
+if a / b == c:
+    print('а разделить на b равно с')
+else:
+    print('а разделить на b не равно с')
+if a ** b == c:
+    print(' а в степени b равно c')
+else:
+    print(' а в степени b не равно с')
