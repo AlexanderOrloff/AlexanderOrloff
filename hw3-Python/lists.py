3a2429857965be2a688da925f1e15e92a9b9b143
AlexanderOrloff <crazymusik98@gmail.com>
2016-11-13 01:18:05 +0300

    Add files via upload

---
+++ hw3-Python/lists.py
@@ -0,0 +1,10 @@
+#вариант7
+count = 0
+arr = ['','','','']
+while  count  < 4:
+    s = input('vvedi slovo')
+    arr [ int(count)] += s
+    s = ''
+    count += 0.5
+for i in range (0,4):
+    print(arr[i])
