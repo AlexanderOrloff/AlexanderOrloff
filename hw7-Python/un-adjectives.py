1dced71185e56e54a5a2b364cf1666d3a5c6dc8a
AlexanderOrloff <crazymusik98@gmail.com>
2017-01-13 12:49:54 +0300

    Add files via upload

---
+++ hw7-Python/un-adjectives.py
@@ -0,0 +1,35 @@
+# вариант7
+def open_text(text):
+    words = []
+    with open(text, 'r', encoding ='utf-8') as f:
+        text = f.read().lower()
+        text = text.split()
+        for item in text:
+            item = item.strip('.,?!-')
+            if item not in words:
+                words.append(item)
+    return words
+
+def percent(words, number):
+    i,j = 0,0
+    for item in words:
+        if item[0:2] =='un':
+            i+=1
+            if len(item) > number:
+                j +=1   
+    if i != 0:
+        return round(j / i * 100)
+    else:
+        return 'no matching words were found'
+
+def questions():
+    yourwords = []
+    text = input(' Please, enter the name of the text')
+    number = int(input(' Please, enter the lenght'))
+    words = open_text(text)
+    answer = percent(words, number)
+    return answer
+
+print(questions())
+    
+
