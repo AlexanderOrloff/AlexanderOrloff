3ef1c43c78400a4763e9053a911fe468e3af02ad
AlexanderOrloff <crazymusik98@gmail.com>
2017-02-10 20:52:03 +0300

    Update ptitsi.py

--- hw11-Python/ptitsi.py
+++ hw11-Python/ptitsi.py
@@ -1,12 +1,17 @@
 import re
 
-def substitute():
+def open():
     with open('ptitsi.html','r', encoding = 'utf-8') as f:
         content = f.read()
+    return content
+def substitute(content):         
     content = re.sub('<.*?>','', content, flags = re.DOTALL) #delete html tags
     content = re.sub(r'(\n| ){2,}','' ,content, flags = re.DOTALL) #delete html tags
     content = re.sub('птиц(а(ми?|х)|ы|е(й|ю)?|у)?','рыб\\1', content)
     content = re.sub('Птиц(а(ми?|х)|ы|е(й|ю)?|у)?','Рыб\\1', content)
     return content
+def write(content):
+    with open('text.txt','w', encoding = 'utf-8') as f:
+        f.write(content)
 
-print(substitute())
+print(write(substitute(open()))
