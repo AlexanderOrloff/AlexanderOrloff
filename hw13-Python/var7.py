c13f1355aa3ce20dec2b03628552778b934957b4
AlexanderOrloff <crazymusik98@gmail.com>
2017-04-13 10:17:00 +0300

    Update var7.py

--- hw13-Python/var7.py
+++ hw13-Python/var7.py
@@ -7,6 +7,8 @@ def search():
     a =[]
     for f in os.listdir():
         if  os.path.isdir(f) and f not in a:
+            #в принципе, так как у меня только папки в задании совпадений не будет, так что массив не очень оправдвн, конечно
+            #но потом будет можно при работе с файлами отделить функцией re всё, что до точки
             lat = re.search('.*[a-zA-z].*', str(f))
             rus = re.search('.*[а-яА-ЯЁё].*', str(f))
             if lat != None and rus != None:
